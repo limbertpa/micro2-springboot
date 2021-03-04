5c00c0b65293f223dea2078f29969ed449e7e491
limbertp <limbertarceperez@gmail.com>
2021-03-04 16:52:32 -0400

    init project

---
+++ discovery-server/src/test/java/com/ston/app/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ston.app.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
